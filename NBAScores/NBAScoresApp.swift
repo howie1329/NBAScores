b5c6c5fc300e2dc2e30535e2378b406e8679aa6f
howie1329 <46204910+howie1329@users.noreply.github.com>
2023-03-03 01:58:24 -0500

    Initial Commit

---
+++ NBAScores/NBAScoresApp.swift
@@ -0,0 +1,17 @@
+//
+//  NBAScoresApp.swift
+//  NBAScores
+//
+//  Created by Howard Thomas on 3/3/23.
+//
+
+import SwiftUI
+
+@main
+struct NBAScoresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
